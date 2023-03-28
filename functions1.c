35b0d7021f55ea0797f55df1ae010c3c37b77392
Ndinie28 <122854041+Ndinie28@users.noreply.github.com>
2023-03-28 18:52:31 +0200

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -6,6 +6,7 @@
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
+ * active flags should be calculated
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
